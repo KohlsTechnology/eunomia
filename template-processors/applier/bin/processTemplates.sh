da3cc50c4e7f112b2f8ebbbd71d49deb1b919801
Sean Malloy <sean.malloy@kohls.com>
2019-10-29 17:11:26 -0500

    Prepare for using GitHub pages

--- template-processors/applier/bin/processTemplates.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset
-set -o errexit
-
-cd $CLONED_TEMPLATE_GIT_DIR/
-ansible-galaxy install -r requirements.yml -p galaxy
-ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml
