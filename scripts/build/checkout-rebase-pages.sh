a0e2cbc90297ca5f1e59d07fe2f3239f9873c1d3
Mateusz Kyc <mateusz.k@kloud9.nyc>
2019-10-31 17:49:56 +0100

    Update TravisCI helm publishing related scripts
    This commit updates scripts/build scripts related to publishing of helm charts after creation of tag in repo. In scripts/build/build-chart-repo.sh it removes line templating values.yaml file as we are not templating it in current chart. In scripts/build/push-to-pages.sh it sets git to explicitly use github_deploy_key while connecting to GitHub.

--- scripts/build/checkout-rebase-pages.sh
+++ scripts/build/checkout-rebase-pages.sh
@@ -1,5 +1,7 @@
-echo GITHUB_PAGES_DIR: $GITHUB_PAGES_DIR
-mkdir -p $GITHUB_PAGES_DIR
+#!/usr/bin/env bash
+set -euxo pipefail
+
+echo GITHUB_PAGES_DIR: "$GITHUB_PAGES_DIR"
+mkdir -p "$GITHUB_PAGES_DIR"
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
-# git -C $GITHUB_PAGES_DIR clone -b "$GITHUB_PAGES_BRANCH" "https://github.com/$GITHUB_PAGES_REPO" .
-git -C $GITHUB_PAGES_DIR clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
+git -C "$GITHUB_PAGES_DIR" clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
