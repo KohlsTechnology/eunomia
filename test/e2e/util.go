ea513319600d510413f0ea8befba7b2a01493c38
Mateusz Czapliński <mateusz.c@kloud9.nyc>
2019-11-21 12:09:36 +0100

    test/e2e: fix and refactor GetPod and its callers
    The WaitForPodWithImage function had a bug, where it was written as if
    GetPod could return an error, though it could not. Comparing it with a
    similar WaitForPod func, it looked useful to allow returning an error
    from GetPod, to help in debugging if needed.
    
    Also, the WaitFor... funcs are refactored to make them easier to
    understand.

--- test/e2e/util.go
+++ test/e2e/util.go
@@ -34,22 +34,22 @@ import (
 
 // GetPod retrieves a given pod based on namespace, the pod name prefix, and the image used
 // Original Source https://github.com/jaegertracing/jaeger-operator/blob/master/test/e2e/utils.go
-func GetPod(namespace, namePrefix, containsImage string, kubeclient kubernetes.Interface) v1.Pod {
+func GetPod(namespace, namePrefix, containsImage string, kubeclient kubernetes.Interface) (*v1.Pod, error) {
 	pods, err := kubeclient.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 	if err != nil {
-		return v1.Pod{}
+		return nil, err
 	}
 	for _, pod := range pods.Items {
 		if strings.HasPrefix(pod.Name, namePrefix) {
 			for _, c := range pod.Spec.Containers {
-				fmt.Printf("Found pod %s", c.Image)
+				fmt.Printf("Found pod %s\n", c.Image)
 				if strings.Contains(c.Image, containsImage) {
-					return pod
+					return &pod, nil
 				}
 			}
 		}
 	}
-	return v1.Pod{}
+	return nil, nil
 }
 
 // WaitForPod retrieves a specific pod with a known name and namespace and waits for it to be running and available
@@ -58,18 +58,18 @@ func WaitForPod(t *testing.T, f *framework.Framework, ctx *framework.TestCtx, na
 		// Check if the CRD has been created
 		pod := &v1.Pod{}
 		err = f.Client.Get(goctx.TODO(), types.NamespacedName{Name: name, Namespace: namespace}, pod)
-		if err != nil {
-			if apierrors.IsNotFound(err) {
-				t.Logf("Waiting for availability of %s pod\n", name)
-				return false, nil
-			}
+		switch {
+		case apierrors.IsNotFound(err):
+			t.Logf("Waiting for availability of %s pod", name)
+			return false, nil
+		case err != nil:
 			return false, err
-		}
-		if pod.Status.Phase == "Running" {
+		case pod.Status.Phase == "Running":
 			return true, nil
+		default:
+			t.Logf("Waiting for full availability of %s pod", name)
+			return false, nil
 		}
-		t.Logf("Waiting for full availability of %s pod\n", name)
-		return false, nil
 	})
 	if err != nil {
 		return err
@@ -82,23 +82,23 @@ func WaitForPod(t *testing.T, f *framework.Framework, ctx *framework.TestCtx, na
 func WaitForPodWithImage(t *testing.T, f *framework.Framework, ctx *framework.TestCtx, namespace, name string, image string, retryInterval time.Duration, timeout time.Duration) error {
 	err := wait.Poll(retryInterval, timeout, func() (done bool, err error) {
 		// Check if the CRD has been created
-		pod := GetPod(namespace, name, image, f.KubeClient)
-		if err != nil {
-			if apierrors.IsNotFound(err) {
-				t.Logf("Waiting for availability of %s pod\n", name)
-				return false, nil
-			}
+		pod, err := GetPod(namespace, name, image, f.KubeClient)
+		switch {
+		case apierrors.IsNotFound(err):
+			t.Logf("Waiting for availability of %s pod", name)
+			return false, nil
+		case err != nil:
 			return false, err
-		}
-		if pod.Status.Phase == "Running" {
+		case pod != nil && pod.Status.Phase == "Running":
 			return true, nil
+		default:
+			t.Logf("Waiting for full availability of %s pod", name)
+			return false, nil
 		}
-		t.Logf("Waiting for full availability of %s pod\n", name)
-		return false, nil
 	})
 	if err != nil {
 		return err
 	}
-	t.Logf("pod available\n")
+	t.Logf("pod %s in namespace %s is available", name, namespace)
 	return nil
 }
