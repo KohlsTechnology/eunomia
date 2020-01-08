b12bece4a807ea6bb16201235d03352abb91cc26
Szymon Bar <sbar@griddynamics.com>
2020-01-08 11:58:22 +0100

    Add e2e build tag to end-to-end test files
    The build tag makes "go test ./..." work again. e2e build tag tags the
    files to be excluded from default tests.
    
    Also, add e2e.go file with package docs to avoid an error of excluding
    all go files from e2e package.
    
    Reference:
    https://stackoverflow.com/a/47538114

---
+++ test/e2e/e2e.go
@@ -0,0 +1,7 @@
+// Package e2e contains end-to-end eunomia tests
+package e2e
+
+// The file is left as a placeholder - other files of package e2e are
+// excluded from default tests by a build tag; at least one file from
+// a package has to be without a tag for an error not to be thrown.
+// See: https://stackoverflow.com/a/47538114
