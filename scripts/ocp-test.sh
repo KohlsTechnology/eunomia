938807e91e876891d686daf47aeeccb67ec0e27b
Mateusz Czapliński <mateusz.c@kloud9.nyc>
2020-02-11 12:34:23 +0100

    scripts/ocp-test.sh: use $TEST_NAMESPACE instead of hardcoded name
    Fix hardcoded occurences of "test-eunomia-operator" to use predefined
    TEST_NAMESPACE env var instead. This makes it more clear which commands
    need to use exactly the same namespace.
    
    Also, unify the format how the env var is referenced vs. other env vars
    (double quotes and curly braces).

--- scripts/ocp-test.sh
+++ scripts/ocp-test.sh
@@ -28,14 +28,14 @@ minishift status || {
 eval "$(minishift oc-env)"
 # FIXME: sort out permissions; for now using admin, to have something working as a first step
 oc login -u system:admin
-if ! oc get projects | grep -q test-eunomia-operator; then
-    oc new-project test-eunomia-operator
+if ! oc get projects | grep -q "${TEST_NAMESPACE}"; then
+    oc new-project "${TEST_NAMESPACE}"
 fi
-oc project test-eunomia-operator
+oc project "${TEST_NAMESPACE}"
 
 # Ensure clean workspace
-if [[ $(oc get namespace $TEST_NAMESPACE) ]]; then
-    oc delete namespace $TEST_NAMESPACE
+if [[ $(oc get namespace "${TEST_NAMESPACE}") ]]; then
+    oc delete namespace "${TEST_NAMESPACE}"
 fi
 
 eval "$(minishift docker-env)"
@@ -46,12 +46,12 @@ helm template deploy/helm/eunomia-operator/ \
     --set eunomia.operator.openshift.enabled=true \
     --set eunomia.operator.image.tag=dev \
     --set eunomia.operator.image.pullPolicy=Never \
-    --set eunomia.operator.namespace=$TEST_NAMESPACE | oc apply -f -
+    --set eunomia.operator.namespace="${TEST_NAMESPACE}" | oc apply -f -
 
 # Deployment test
-oc wait --for=condition=available --timeout=30s deployment/eunomia-operator -n $TEST_NAMESPACE
-podname=$(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' -n $TEST_NAMESPACE)
-if oc exec "${podname}" date -n $TEST_NAMESPACE; then
+oc wait --for=condition=available --timeout=30s deployment/eunomia-operator -n "${TEST_NAMESPACE}"
+podname=$(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' -n "${TEST_NAMESPACE}")
+if oc exec "${podname}" date -n "${TEST_NAMESPACE}"; then
     echo "Eunomia deployment successful"
 else
     echo "Eunomia deployment failed"
@@ -59,13 +59,14 @@ else
 fi
 
 ## Testing templates-processors/ocp-template
+# FIXME: we should put GitOpsConfig in a different namespace than eunomia-operator itself
 
 #Test ocp-hello-cr1
 ocp_hello_cr1() {
     timeout=30
     # FIXME: below CR should be modified by the script to use PR branch ref instead of master
     oc apply -f test/e2e/testdata/simple/ocp-hello-cr1.yaml
-    while ((--timeout)) && [[ "$(oc get po -n test-eunomia-operator -l app=helloworld -o=jsonpath="{range .items[*]}{.status.phase}{'\n'}{end}")" != "Running" ]]; do
+    while ((--timeout)) && [[ "$(oc get po -n "${TEST_NAMESPACE}" -l app=helloworld -o=jsonpath="{range .items[*]}{.status.phase}{'\n'}{end}")" != "Running" ]]; do
         echo "waiting for ocp-hello-cr1 deployment: remaining $timeout sec..."
         sleep 1
     done
@@ -81,5 +82,4 @@ ocp_hello_cr1
 # FIXME: the helloworld pod is still alive after below line; ownedKinds is empty, probably because missing RBAC policies
 oc delete -f test/e2e/testdata/simple/ocp-hello-cr1.yaml
 
-oc delete namespace test-eunomia-operator
-
+oc delete namespace "${TEST_NAMESPACE}"
