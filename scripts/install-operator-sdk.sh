b2d8d8307f327a419fcc11b0e918474c4a4a94ec
Heiko Zuerker <heiko@zuerker.org>
2020-05-13 10:03:34 -0500

    add script to install correct operator-sdk version

---
+++ scripts/install-operator-sdk.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 Kohl's Department Stores, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euxo pipefail
+
+RELEASE_VERSION=${1}
+TARGET_PATH=${2}
+
+OS="$(uname)"
+
+if [ "${OS}" = "Darwin" ]; then
+    curl -OJL "https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin"
+    chmod +x "operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin" && cp "operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin" "${TARGET_PATH}/operator-sdk" && rm "operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin"
+elif [ "${OS}" == "Linux" ]; then
+    curl -OJL "https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu"
+    chmod +x "operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu" && cp "operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu" "${TARGET_PATH}/operator-sdk" && rm "operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu"
+else
+    echo "Sorry, your OS is currently not supported. We'll happily accept a pull request to add it... :)"
+fi
